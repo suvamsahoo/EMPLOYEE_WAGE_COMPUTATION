7344e664020ae197444edead2816a86103f13629
suvamsahoo <suvamsahoo77@gmail.com>
2021-03-03 17:15:32 +0530

    UC-4 use case statement to calculate wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,23 +5,46 @@ echo "------------------------- Welcome To Employee Wage Compution -------------
 #VALUE TO ASSING PRESENT
 isPresent=1
 
-#TO GET RANDOM VALUE
+#GET RANDOM VALUE
 randomNumber=$((RANDOM%2))
 
-#FUNCTION TO CALCULATE PART TIME EMPLOYEE AND  WAGE-:
-        function partTimeEmployeeAndWage(){
-        read -p "Assume Wage per hour is :" isWagePerHour "rupees"
-        read -p "To part time hour is :" isPartTimeHour
-        calculatePartTimeEmployeeWage=$(( $isWagePerHour * $isPartTimeHour ))
-        echo $calculatePartTimeEmployeeWage #return
+WagePerHour=20;
+FullDayHour=8;
+PartTimeHour=4;
+
+#FUNCTION TO CALCULATE EMPLOYEE WAGE-:
+function calculateDailyEmployeeWae(){
+      calculateEmployeeWage=$(( $WagePerHour * $FullDayHour ))
+      echo $calculateEmployeeWage #return
 }
 
-#CHECK THE EMPLOYEE PRESENT AND ABSENT-:
+#FUNCTION TO CALCULATE PART TIME EMPLOYEE WAGE-:
+function partTimeEmployeeAndWage(){
+      calculatePartTimeEmployeeWage=$(( $WagePerHour * $PartTimeHour ))
+      echo $calculatePartTimeEmployeeWage #return
+}
+
+#CHECK THE EMPLOYEE IS PRESENT AND ABSENT-:
 if [ $randomNumber -eq 	$isPresent ]
 then
 	echo "Employee Present"
-        wage="$( partTimeEmployeeAndWage )"
-        echo "Calculate part time employee and wage :" $wage "rupees"
+        caseChoce=$((RANDOM%2));
+
+  #case statement-:
+  case "$caseChoce" in
+	0)
+		DailyWage="$( calculateDailyEmployeeWae )"
+                echo "Employee daily wage :" $DailyWage "rupees"
+		;;
+	1)
+                PartTimeWage="$( partTimeEmployeeAndWage )"
+                echo "Calculate part time employee and wage :" $PartTimeWage "rupees"
+		;;
+
+	*)      echo "No choice"
+		;;
+   esac
+
 else
    echo "Employee Absent"
 fi
