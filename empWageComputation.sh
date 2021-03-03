6c76184378c5052b3bc8bfeffc19c1da3f445bb9
suvamsahoo <suvamsahoo77@gmail.com>
2021-03-04 04:25:07 +0530

    UC-7 Store daily wage along with total wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,46 +2,50 @@
 
 echo "------------------------- Welcome To Employee Wage Compution ------------------------"
 
-#VALUE TO ASSING PRESENT
-isPresent=1
-
-#GET RANDOM VALUE
-randomNumber=$((RANDOM%2))
-
 EMPLOYEE_HOUR_FULLTIME=8
 EMPLOYEE_HOUR_PARTTIME=4
 CompanyTotalWorkingDays=20; #In a month
 CompanyTotalEmployeeHours=100;
 WagePerHour=20;
-TotalSalary=0;
+TotalWage=0;
 TotalEmployeeHours=0;
 TotalWorkingDays=0;
 
+#FUNCTION TO GET WORK HOURS
+function getWorkHours()
+{
+  caseChoce=$((RANDOM%3))
+  case $caseChoce in
+	0)
+	  employeeHour=$((EMPLOYEE_HOUR_FULLTIME))
+	  ;;
+        1)
+          employeeHour=$((EMPLOYEE_HOUR_PARTTIME))
+	  ;;
+	*)
+	  employeeHour=0
+          ;;
+   esac
+   echo $employeeHour #return
+}
+
+#FUNCTION TO CALCULATE WAGE
+function calculateWage()
+{
+  employeeHR=$1
+  wage=$(($employeeHR * $WagePerHour))
+  echo $wage #return
+}
 
 while [[ $TotalEmployeeHours -le $CompanyTotalEmployeeHours &&
                $TotalWorkingDays -le $CompanyTotalWorkingDays ]]
 do
       ((TotalWorkingDays++))
-      caseChoce=$((RANDOM%3));
 
-      #case statement-:
-      case "$caseChoce" in
-	     0)
-                employeeHour=$((EMPLOYEE_HOUR_FULLTIME))
-		;;
-	     1)
-                employeeHour=$((EMPLOYEE_HOUR_PARTTIME))
-		;;
-
-	     *)
-                employeeHour=0
-		;;
-     esac
-
-     TotalEmployeeHours=$(($TotalEmployeeHours + $employeeHour))
+      dailyHR="$(getWorkHours)"
+      dailyWage="$(calculateWage $dailyHR)"
+      TotalWage=$(($TotalWage + $dailyWage))
+      TotalEmployeeHours=$(($TotalEmployeeHours + $dailyHR))
 done
 
-#SALARY IN A MONTH
-TotalWorkingWage=$(($TotalEmployeeHours * $WagePerHour))
-echo "In this month employee's total working hours is $TotalEmployeeHours & total working wage is $TotalWorkingWage"
-
+echo "In this month employee total working hours is $TotalEmployeeHours & total wage is $TotalWage"
